37b4f9d5661cc762b41ed03a7b3ffee30fab8a00
miminouvel <mhicaellajlopez@gmail.com>
2021-05-06 01:00:15 +0000

    w5d2_correct

--- app/actions.rb
+++ app/actions.rb
@@ -54,9 +54,23 @@ end
   end
 
   get '/finstagram_posts/new' do
+    @finstagram_post = FinstagramPost.new
     erb(:"finstagram_posts/new")
   end
+  
+  post '/finstagram_posts' do
+    photo_url = params[:photo_url]
+  
+    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+  
+    if @finstagram_post.save
+      redirect(to('/'))
+    else
+      erb(:"finstagram_posts/new")
+    end
+  end
 
-post '/finstagram_posts' do
-    params.to_s
+get '/finstagram_posts/:id' do
+  @finstagram_post = FinstagramPost.find(params[:id])   # find the finstagram post with the ID from the URL
+  erb(:"finstagram_posts/show")               # render app/views/finstagram_posts/show.erb
 end
